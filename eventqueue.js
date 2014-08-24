bfa8349068bdfdae6dc3a3ded1aaeedf5087afe8
VirtuosoChris <chris@virtuosoengine.com>
2014-08-24 15:50:15 -0400

    Update eventqueue.js

--- eventqueue.js
+++ eventqueue.js
@@ -1,48 +1,50 @@
-function addPotentialDecision(nation, turnToExecute, choiceString, canExecuteFun, statusString, executeFun)
-{
-
-	nation.upcomingDecisions.push([turnToExecute, choiceString, canExecuteFun, statusString, executeFun]);
-
-}
-
+//event is [turn, functionToCall, dayString]
+//decision is [turn, choicestring, canExecuteFun, statusString, executeFun]
 
 var EVENT_TURN_INDEX = 0;
 var EVENT_FUNCTION_INDEX=1;
 var EVENT_DAY_STRING=2;
+
+var DECISION_TURN_INDEX=0;
+var DECISION_CHOICE_STRING_INDEX=1;
+var DECISION_CAN_EXECUTE_FUN_INDEX=2;
+var DECISION_STATUS_STRING_INDEX=3;
+var DECISION_EXECUTE_FUN_INDEX=4;
+
+function addPotentialDecision(nation, turnToExecute, choiceString, canExecuteFun, statusString, executeFun)
+{
+	nation.upcomingDecisions.push([turnToExecute, choiceString, canExecuteFun, statusString, executeFun]);
+}
 
 function addUpcomingEvent(nation, onTurn, fToCall, dayString)
 {
 	nation.upcomingEvents.push([onTurn, fToCall, dayString]);
 }
 
-
-
-//event is [turn, functionToCall, dayString]
 function fireEvent(event)
 {
 	event[EVENT_FUNCTION_INDEX]();
 	status += event[EVENT_DAY_STRING];
+}
+
+function wrapDecision(decision)
+{
+	return function(){fireDecision(decision);};
+}
+
+function fireDecision(decision)
+{
 
 }
 
-
-var DECISION_TURN_INDEX=0;
-var DECISION_CHOICE_STRING_INDEX=1;
-var DECISION_CAN_EXECUTE_FUN_INDEX=2;
-var DECISION_STATUS_STRING_INDEX=3;
-var DECISION_EXECUTE_FUN=4;
-
-//decision is [turn, choicestring, canExecuteFun, statusString, executeFun]
-function fireDecision(decision)
+function pushDecision()
 {
-	if(decision[CAN_EXECUTE_FUN_INDEX]())
+	if(decision[DECISION_CAN_EXECUTE_FUN_INDEX]())
 	{
-	  
-	  decision[DECISION_EXECUTE_FUN]();
-	
-	}
-	
-	
+	  status += decision[DECISION_STATUS_STRING_INDEX];
+	  currentDecisionFunctions.push(decision[DECISION_EXECUTE_FUN_INDEX]);
+	  currentDecisionStrings.push(decision[DECISION_CHOICE_STRING_INDEX]);
+	}	
 }
 
 
@@ -53,7 +55,6 @@ function eraseEvent(nation, event)
   if (index > -1) {
     nation.upcomingEvents.splice(index, 1);
   }
-
 }
 
 function eraseDecision(nation, event)
@@ -63,41 +64,33 @@ function eraseDecision(nation, event)
   if (index > -1) {
     nation.upcomingDecisions.splice(index, 1);
   }
-
 }
 
 
 function checkForTriggeredEvents(nation)
 {
- 
   for(i =0; i < nation.upcomingEvents.length; i++)
   {
 	event = nation.upcomingEvents[i];
 
 	if(event[0] <= nation.turnCount)
 	{	
-	   
 	   fireEvent(event);
 	   eraseEvent(nation, event);
 	}	
   }
-
 }
-
 
 function checkForCurrentDecisions()
 {
-	
 	for(i =0; i < nation.upcomingDecisions.length; i++)
 	{
 		decision = nation.upcomingDecisions[i];
 
 		if(decision[0] <= nation.turnCount)
 		{
-			fireDecision(decision);
+			pushDecision(decision);
 			eraseDecision(nation,decision); 
 		}
-
 	}
-
 }
