1db8f47295b8cf099ecef779ef654318a16446d5
VirtuosoChris <chris@virtuosoengine.com>
2014-08-24 14:50:57 -0400

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,11 @@
+//game specific strings and string generator functions
+
+var prologue = ""; //prologue paragraph for the game
+
+
+//generates a unique game loss state string based on the current game state
+function lossString()
+{
+
+
+}
