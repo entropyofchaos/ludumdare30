4a56a13394a409f28a857d623bc3c1bf637ea8bd
VirtuosoChris <chris@virtuosoengine.com>
2014-08-24 15:04:20 -0400

    Update strings.js

--- strings.js
+++ strings.js
@@ -2,6 +2,14 @@
 
 var prologue = ""; //prologue paragraph for the game
 
+var riotString1 = "The people are discontented.  Riots have begun.";
+
+var riotString2 = "The rioting has continued.  The violence has spread far and wide.  Your forces are mobilizing in an effort to maintain control.";
+
+var riotString3 = "Despite a strong show of force, you have been unable to suppress the growing dissent among your people.";
+
+var riotStrings = [riotString1, riotString2, riotString3];
+
 
 //generates a unique game loss state string based on the current game state
 function lossString()
