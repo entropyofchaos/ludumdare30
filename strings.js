7f3fe4d989dd47aa778ef1c8f3d964b5bf51e6ed
VirtuosoChris <chris@virtuosoengine.com>
2014-08-24 17:45:37 -0400

    Update strings.js

--- strings.js
+++ strings.js
@@ -27,6 +27,38 @@ var assimilationString3 = "";
 var assimilationStrings = [assimilationString1, assimilationString2, assimilationString3];
 
 
+function approvalString(nation)
+{
+  if(approvalStrong(nation))
+  {
+    return "Your people revere you.";
+  }
+  else if(approvalWeak(nation))
+  {
+    return "Your people despise you."
+  }
+  
+  return "";
+}
+
+
+function appendOtherStatusStrings(nation)
+{
+  if(cultureStrong(nation))
+  {
+    status += "Pride in the nation is keeping your people united."
+  }
+  
+  if(industryStrong(nation))
+  {
+    status += "Your industrial machine is the envy of the world."
+  }
+  
+  status += approvalString(nation);
+  
+}
+
+
 //generates a unique game loss state string based on the current game state
 function lossString(nation)
 {
