2c0928359bdf55dc579827e1a03cda59ef62c509
VirtuosoChris <chris@virtuosoengine.com>
2014-08-24 14:55:36 -0400

    Update gamestate.js

--- gamestate.js
+++ gamestate.js
@@ -95,35 +95,6 @@ function cultureMoribund()
 }
 
 
-//returns an array containing [paragraph, decisionstrings, decisionfunctions]
-function nextTurn()
-{	
-	currentDecisions=[];
-	status = "";
-	
-	currentNation.turnCount++;
-
-	currentNation = nations[(currentNation+1)%2];
-
-	if(isFamine(currentNation))
-	{
-	  currentNation.famineCounter++;
-	}
-
-	if(isRiots(currentNation))
-	{
-	  currentNation.riotCounter++;
-	}
-
-	if(cultureMoribund(currentNation))
-	{
-	  currentNation.assimilationCounter++;
-	}
-
-	
-}
-
-
 function canTrade()
 {
    return internationalRelations > .4;
