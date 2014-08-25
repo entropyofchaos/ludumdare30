9612656d65b269b305f35151ac07bc2bf872f048
entropyofchaos <entropyofchaos@gmail.com>
2014-08-24 21:05:00 -0400

    Merging stuff

--- initGame.js
+++ initGame.js
@@ -3,9 +3,16 @@ function initGame()
   
 	addUpcomingEvent(nations[0], 0, function(){}, prologue);
 
-	addUpcomingEvent(nations[0], 0, function(){}, rothingrad);
+	addUpcomingEvent(nations[0], 0, function(){play('jrasic1.wav')}, rothingrad);
   
-	addUpcomingEvent(nations[1], 1, function(){}, saurian);
+	addUpcomingEvent(nations[1], 0, function(){play('raptor1.wav')}, saurian);
 }
 
-
+function play(song) 
+{
+  var embed=document.createElement('object');
+  embed.setAttribute('type','audio/wav');
+  embed.setAttribute('data', song);
+  embed.setAttribute('autostart', true);
+  document.getElementsByTagName('body')[0].appendChild(embed);
+}
