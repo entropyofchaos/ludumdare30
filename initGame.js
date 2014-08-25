69e7eaab6b130143e9a15ae04a5418371ec565c8
entropyofchaos <entropyofchaos@gmail.com>
2014-08-24 21:26:26 -0400

    Audio aswomeness

--- initGame.js
+++ initGame.js
@@ -3,16 +3,13 @@ function initGame()
   
 	addUpcomingEvent(nations[0], 0, function(){}, prologue);
 
-	addUpcomingEvent(nations[0], 0, function(){play('jrasic1.wav')}, rothingrad);
+	addUpcomingEvent(nations[0], 0, function(){}, rothingrad);
   
-	addUpcomingEvent(nations[1], 0, function(){play('raptor1.wav')}, saurian);
+	addUpcomingEvent(nations[1], 0, function(){play('intoTheme');play('raptorGrowl');}, saurian);
 }
 
 function play(song) 
 {
-  var embed=document.createElement('object');
-  embed.setAttribute('type','audio/wav');
-  embed.setAttribute('data', song);
-  embed.setAttribute('autostart', true);
-  document.getElementsByTagName('body')[0].appendChild(embed);
+	var audio = document.getElementById(song);
+	audio.play();
 }
